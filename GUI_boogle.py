f7dfd946a05b16aef65c5cf536f748fe3bc0491d
amitaiturkel <amitai.turkel@gmail.com>
2023-01-12 21:25:15 +0200

    Merge branch 'main' of https://github.com/ElsaMarziano/ex11
    # Conflicts:
    #	generic_functions.py

--- GUI_boogle.py
+++ GUI_boogle.py
@@ -28,6 +28,7 @@ class App:
         start_button["font"] = ft
         start_button["fg"] = "#000000"
         start_button["justify"] = "center"
+        self.start_button = start_button
 
 
         Button_3_0=tk.Button(root)
@@ -37,9 +38,10 @@ class App:
         Button_3_0["font"] = ft
         Button_3_0["fg"] = "#000000"
         Button_3_0["justify"] = "center"
-        Button_3_0["text"] = str(self.board[3][0])
+        Button_3_0["text"] = str(self.board[0][3])
         Button_3_0.place(x=250,y=170,width=70,height=70)
         Button_3_0["command"] = self.Button_0_3_command
+        self.Button_3_0 = Button_3_0
 
         Button_1_3=tk.Button(root)
         Button_1_3["activeforeground"] = "#ebe6ca"
@@ -51,6 +53,7 @@ class App:
         Button_1_3["text"] = str(self.board[1][3])
         Button_1_3.place(x=250,y=250,width=70,height=70)
         Button_1_3["command"] = self.Button_1_3_command
+        self.Button_1_3 = Button_1_3
 
         Button_2_3=tk.Button(root)
         Button_2_3["activeforeground"] = "#ebe6ca"
@@ -62,6 +65,7 @@ class App:
         Button_2_3["text"] = str(self.board[2][3])
         Button_2_3.place(x=250,y=330,width=70,height=70)
         Button_2_3["command"] = self.Button_2_3_command
+        self.Button_2_3 = Button_2_3
 
         Button_3_3=tk.Button(root)
         Button_3_3["activeforeground"] = "#ebe6ca"
@@ -73,6 +77,7 @@ class App:
         Button_3_3["text"] = str(self.board[3][3])
         Button_3_3.place(x=250,y=410,width=70,height=70)
         Button_3_3["command"] = self.Button_3_3_command
+        self.Button_3_3 = Button_3_3
 
         Button_0_2=tk.Button(root)
         Button_0_2["activeforeground"] = "#ebe6ca"
@@ -84,6 +89,7 @@ class App:
         Button_0_2["text"] = str(self.board[0][2])
         Button_0_2.place(x=170,y=170,width=70,height=70)
         Button_0_2["command"] = self.Button_0_2_command
+        self.Button_0_2 = Button_0_2
 
         Button_1_2=tk.Button(root)
         Button_1_2["activeforeground"] = "#ebe6ca"
@@ -95,6 +101,7 @@ class App:
         Button_1_2["text"] = str(self.board[1][2])
         Button_1_2.place(x=170,y=250,width=70,height=70)
         Button_1_2["command"] = self.Button_1_2_command
+        self.Button_1_2 = Button_1_2
 
         Button_2_2=tk.Button(root)
         Button_2_2["activeforeground"] = "#ebe6ca"
@@ -106,6 +113,7 @@ class App:
         Button_2_2["text"] = str(self.board[2][2])
         Button_2_2.place(x=170,y=330,width=70,height=70)
         Button_2_2["command"] = self.Button_2_2_command
+        self.Button_2_2 = Button_2_2
 
         Button_3_2=tk.Button(root)
         Button_3_2["activeforeground"] = "#ebe6ca"
@@ -117,6 +125,7 @@ class App:
         Button_3_2["text"] = str(self.board[3][2])
         Button_3_2.place(x=170,y=410,width=70,height=70)
         Button_3_2["command"] = self.Button_3_2_command
+        self.Button_3_2 = Button_3_2
 
         Button_0_1=tk.Button(root)
         Button_0_1["activeforeground"] = "#ebe6ca"
@@ -128,6 +137,7 @@ class App:
         Button_0_1["text"] = str(self.board[0][1])
         Button_0_1.place(x=90,y=170,width=70,height=70)
         Button_0_1["command"] = self.Button_0_1_command
+        self.Button_0_1 = Button_0_1
 
         Button_2_1=tk.Button(root)
         Button_2_1["activeforeground"] = "#ebe6ca"
@@ -139,6 +149,7 @@ class App:
         Button_2_1["text"] = str(self.board[2][1])
         Button_2_1.place(x=90,y=330,width=70,height=70)
         Button_2_1["command"] = self.Button_2_1_command
+        self.Button_2_1 = Button_2_1
 
         Button_3_1=tk.Button(root)
         Button_3_1["activeforeground"] = "#ebe6ca"
@@ -150,6 +161,7 @@ class App:
         Button_3_1["text"] = str(self.board[3][1])
         Button_3_1.place(x=90,y=410,width=70,height=70)
         Button_3_1["command"] = self.Button_3_1_command
+        self.Button_3_1 = Button_3_1
 
         Button_1_1=tk.Button(root)
         Button_1_1["activeforeground"] = "#ebe6ca"
@@ -161,6 +173,7 @@ class App:
         Button_1_1["text"] = str(self.board[1][1])
         Button_1_1.place(x=90,y=250,width=70,height=70)
         Button_1_1["command"] = self.Button_1_1_command
+        self.Button_1_1 = Button_1_1
 
         Button_0_0=tk.Button(root)
         Button_0_0["activeforeground"] = "#ebe6ca"
@@ -172,6 +185,7 @@ class App:
         Button_0_0["text"] = str(self.board[0][0])
         Button_0_0.place(x=10,y=170,width=70,height=70)
         Button_0_0["command"] = self.Button_0_0_command
+        self.Button_0_0 = Button_0_0
 
         Button_1_0=tk.Button(root)
         Button_1_0["activeforeground"] = "#ebe6ca"
@@ -183,6 +197,7 @@ class App:
         Button_1_0["text"] = str(self.board[1][0])
         Button_1_0.place(x=10,y=250,width=70,height=70)
         Button_1_0["command"] = self.Button_1_0_command
+        self.Button_1_0 = Button_1_0
 
         Button_2_0=tk.Button(root)
         Button_2_0["activeforeground"] = "#ebe6ca"
@@ -194,6 +209,7 @@ class App:
         Button_2_0["text"] = str(self.board[2][0])
         Button_2_0.place(x=10,y=330,width=70,height=70)
         Button_2_0["command"] = self.Button_2_0_command
+        self.Button_2_0 = Button_2_0
 
         Button_3_0=tk.Button(root)
         Button_3_0["activeforeground"] = "#ebe6ca"
@@ -205,6 +221,7 @@ class App:
         Button_3_0["text"] = str(self.board[3][0])
         Button_3_0.place(x=10,y=410,width=70,height=70)
         Button_3_0["command"] = self.Button_3_0_command
+        self.Button_3_0 = Button_3_0
 
         Score=tk.Label(root)
         ft = tkFont.Font(family='Ariel',size=28)
@@ -213,7 +230,7 @@ class App:
         Score["justify"] = "center"
         Score["text"] = "socre"
         Score.place(x=0,y=10,width=122,height=33)
-
+        self.Score = Score
 
 
         num_score = tk.Label(root)
@@ -223,7 +240,7 @@ class App:
         num_score["justify"] = "center"
         num_score["text"] = "0"
         num_score.place(x=110,y=15,width=70,height=30)
-
+        self.num_score = num_score
 
         current_word=tk.Label(root)
         current_word["bg"] = "#ecf2f2"
@@ -231,8 +248,9 @@ class App:
         current_word["font"] = ft
         current_word["fg"] = "#01040b"
         current_word["justify"] = "center"
-        current_word["text"] = "current_word"
+        current_word["text"] = ""
         current_word.place(x=130,y=70,width=216,height=30)
+        self.current_word = current_word
 
         submit=tk.Button(root)
         submit["bg"] = "#f0f0f0"
@@ -252,68 +270,70 @@ class App:
 
     def Button_0_3_command(self):
         print("(0,3)")
-        self.choose_letter((0,3))
+        self.update_current_word(board[0][3])
+
 
 
     def Button_1_3_command(self):
         print("(1,3)")
-
+        self.update_current_word(self.board[1][3])
 
     def Button_2_3_command(self):
         print("(2,3)")
-
+        self.update_current_word(self.board[2][3])
 
     def Button_3_3_command(self):
         print("(3,3)")
-
+        self.update_current_word(self.board[3][3])
 
     def Button_0_2_command(self):
         print("(0,2)")
-
+        self.update_current_word(self.board[0][2])
 
     def Button_1_2_command(self):
         print("(1,2)")
-
+        self.update_current_word(self.board[1][2])
 
     def Button_2_2_command(self):
         print("(2,2)")
-
+        self.update_current_word(self.board[2][2])
 
     def Button_3_2_command(self):
         print("(3,2)")
-
+        self.update_current_word(self.board[3][2])
 
     def Button_0_1_command(self):
         print("(0,1)")
+        self.update_current_word(self.board[0][1])
 
 
     def Button_2_1_command(self):
         print("(2,1)")
-
+        self.update_current_word(self.board[2][1])
 
     def Button_3_1_command(self):
         print("(3,1)")
-
+        self.update_current_word(self.board[3][1])
 
     def Button_1_1_command(self):
         print("(1,1)")
-
+        self.update_current_word(self.board[1][1])
 
     def Button_0_0_command(self):
         print("(0,0")
-
+        self.update_current_word(self.board[0][0])
 
     def Button_1_0_command(self):
         print("(1,0")
-
+        self.update_current_word(self.board[1][0])
 
     def Button_2_0_command(self):
         print("(2,0")
-
+        self.update_current_word(self.self.board[2][0])
 
     def Button_3_0_command(self):
         print("(3,0")
-
+        self.update_current_word(self.self.board[3][0])
 
     def submit_command(self):
         print("command")
@@ -335,6 +355,9 @@ class App:
 
     def set_score(self,score):
         num_score["text"] += score
+
+    def update_current_word(self, word):
+        self.current_word.config(text=self.current_word.cget("text") + word)
 
 
 if __name__ == "__main__":
